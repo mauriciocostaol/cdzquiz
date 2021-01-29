86cce63f4aa0d8fd05ad81ab35e6f3c3314e8a89
Mauricio Ricardo <mauricio.costa7@fatec.sp.gov.br>
2021-01-29 20:46:21 -0300

    Código final da aula 03

--- pages/index.js
+++ pages/index.js
@@ -1,46 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import QuizContainer from '../src/components/QuizContainer';
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizBackgound from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GithubCorner';
-// const BackgroundImage = styled.div`
-// background-image: url(${db.bg});
-// flex: 1;
-// background-size: cover;
-// background-position: center;
-// `;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
-
-export const TextBox = styled.input`
-width:18vw;
-height:5vh;
-border-radius:4px;
-border:none;
-`;
-
-export const Button = styled.button`
-background: #f57c00;
-color:#ffffff;
-margin-top: 0.5rem;
-width:18vw;
-height:7vh;
-border-radius: 4px;
-border-color:#76ff03;
-`;
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizLogo from '../src/components/QuizLogo';
 
 export default function Home() {
   const router = useRouter();
@@ -53,26 +22,29 @@ export default function Home() {
 
       </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Os Cavaleiros do Zodiaco</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
 
               router.push(`/quiz?name=${name}`);
             }}
             >
-              <TextBox
-                onChange={function (infosDoEvento) {
+              <Input
+                name="nomeDoUsuario"
+                onChange={(infosDoEvento) => {
                   setName(infosDoEvento.target.value);
                 }}
                 placeholder="Digite seu Nome"
+                value={name}
               />
               <Button type="submit" disabled={name.length === 0}>
-                Jogar &nbsp;
-                { name }
+
+                {`Jogar ${name} `}
               </Button>
             </form>
 
@@ -86,7 +58,7 @@ export default function Home() {
         </Widget>
         <Footer />
       </QuizContainer>
-      <GitHubCorner />
+      <GitHubCorner projectUrl="https://github.com/mauriciocostaol/cdzquiz" />
     </QuizBackgound>
   );
 }
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,10 +1,158 @@
-/* eslint-disable react/jsx-indent */
+/* eslint-disable react/prop-types */
 import React from 'react';
+import styled, { keyframes } from 'styled-components';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
 
-export default function Quiz() {
+const Spin = keyframes`
+to{ transform: rotate(360deg);}
+`;
+
+const Spinner = styled.div`
+border: 8px solid rgba(0,0,0,.1);
+border-left-color:#22a6b3;
+height:60px;
+width:60px;
+border-radius:50%;
+animation:${Spin} 1s linear infinite;
+
+
+`;
+
+function LoadingWidget() {
   return (
-<div>
- Página de Quiz
-</div>
+    <Widget>
+      <Widget.Content>
+        <Spinner />
+        Carregando
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  questionIndex,
+  totalQuestions,
+  onSubmit,
+}) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        {/* <BackLinkArrow href="/" /> */}
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${totalQuestions}`}
+        </h3>
+      </Widget.Header>
+
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <form
+          onSubmit={(infosDoEvento) => {
+            infosDoEvento.preventDefault();
+            onSubmit();
+          }}
+        >
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                as="label"
+                htmlFor={alternativeId}
+              >
+                <input
+                  // style={{ display: 'none' }}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          {/* <pre>
+            {JSON.stringify(question, null, 4)}
+          </pre> */}
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  // [React chama de: Efeitos || Effects]
+  // React.useEffect
+  // atualizado === willUpdate
+  // morre === willUnmount
+  React.useEffect(() => {
+    // fetch() ...
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+  // nasce === didMount
+  }, []);
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questões, parabéns!</div>}
+      </QuizContainer>
+    </QuizBackground>
   );
 }
