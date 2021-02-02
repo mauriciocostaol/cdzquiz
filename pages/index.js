d3837609bb5b92ecee558a607cddbcef77104da7
Mauricio Ricardo <mauricio.costa7@fatec.sp.gov.br>
2021-02-01 22:41:28 -0300

    codigo das aulas 4 e 5

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import QuizContainer from '../src/components/QuizContainer';
 import db from '../db.json';
@@ -10,6 +11,7 @@ import GitHubCorner from '../src/components/GithubCorner';
 import Input from '../src/components/Input';
 import Button from '../src/components/Button';
 import QuizLogo from '../src/components/QuizLogo';
+import Link from '../src/components/Link';
 
 export default function Home() {
   const router = useRouter();
@@ -23,7 +25,17 @@ export default function Home() {
       </Head>
       <QuizContainer>
         <QuizLogo />
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1, y: '0' },
+            hidden: { opacity: 0, y: '100%' },
+
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
             <h1>Os Cavaleiros do Zodiaco</h1>
           </Widget.Header>
@@ -50,11 +62,41 @@ export default function Home() {
 
           </Widget.Content>
         </Widget>
-        <Widget>
+        <Widget
+          as={motion.section}
+          transition={{ delay: 0.5, duration: 0.5 }}
+          variants={{
+            show: { opacity: 1 },
+            hidden: { opacity: 0 },
+          }}
+          initial="hidden"
+          animate="show"
+        >
           <Widget.Header>
-            <h1>Quiz do Pessoal</h1>
+            <h1>Quizzes da Galera</h1>
           </Widget.Header>
-          <p> Outros Quizzes</p>
+          <Widget.Content>
+            <ul>
+              {db.external.map((linkExterno) => {
+                const [projectName, githubUser] = linkExterno
+                  .replace(/\//g, '')
+                  .replace('https:', '')
+                  .replace('.vercel.app', '')
+                  .split('.');
+
+                return (
+                  <li key={linkExterno}>
+                    <Widget.Topic
+                      as={Link}
+                      href={`/quiz/${projectName}___${githubUser}`}
+                    >
+                      {`${githubUser}/${projectName}`}
+                    </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
+          </Widget.Content>
         </Widget>
         <Footer />
       </QuizContainer>
